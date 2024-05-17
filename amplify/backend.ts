70951c273cc16175636af99809b44c8c625fc4af
Justin Crosbie <justincrosbie@gmail.com>
2024-05-17 10:41:15 +1000

    Adds liveness

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -6,3 +6,20 @@ defineBackend({
   auth,
   data,
 });
+
+const livenessStack = createStack("liveness-stack");
+
+const livenessPolicy = new Policy(livenessStack, "LivenessPolicy", {
+  statements: [
+    new PolicyStatement({
+      actions: ["rekognition:StartFaceLivenessSession"],
+      resources: ["*"],
+    }),
+  ],
+});
+auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(livenessPolicy); // allows guest user access
+auth.resources.authenticatedUserIamRole.attachInlinePolicy(livenessPolicy); // allows logged in user access
+
+function createStack(arg0: string) {
+  throw new Error('Function not implemented.');
+}
