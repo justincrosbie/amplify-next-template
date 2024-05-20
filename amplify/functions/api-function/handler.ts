39b9e4aa57f55de7020b93f55e11f54c51bf6866
Justin Crosbie <justincrosbie@gmail.com>
2024-05-20 15:30:49 +1000

    COS issue

--- amplify/functions/api-function/handler.ts
+++ amplify/functions/api-function/handler.ts
@@ -8,7 +8,16 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     headers: {
       "Access-Control-Allow-Origin": "*", // Restrict this to domains you trust
       "Access-Control-Allow-Headers": "*", // Specify only the headers you need to allow
+      "Access-Control-Allow-Methods": "GET, OPTIONS", // Specify only the headers you need to allow
+      "Access-Control-Allow-Credentials": "true", // Specify only the headers you need to allow
     },
     body: JSON.stringify("Hello from myFunction!"),
   };
 };
+
+/*
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+*/
